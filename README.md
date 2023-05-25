cedfae80727f4dc256a73d808262f2e549db52a3
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-05-24 20:17:17 -0500

    API added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Unit1_Project
+
+<!--  #h1, ##h2, ###h3, etc. -->
+
+<!-- link text -->
+[link text](url)
+
+<!-- link photo -->
+![Alt Text](url)
--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const body = $('body')
+const $body = $('body')
 console.log($body)
 
 
+
+ 
+$.ajax(`https://acnhapi.com/v1/bugs/`)
+        .then(response => console.log(response))
+ 
