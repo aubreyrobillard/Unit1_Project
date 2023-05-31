003a5ad167435d217df87c97b2dc7fd670b24394
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-05-30 20:36:03 -0500

    this is the safe spot

--- app.js
+++ app.js
@@ -4,6 +4,42 @@ console.log($body)
 
 
  
-$.ajax(`https://acnhapi.com/v1/bugs/`)
-        .then(response => console.log(response))
+const $searchForm = $('form')
  
+$searchForm.on('submit', event => {
+        
+        // prevents refreshign of page after search
+        event.preventDefault()
+        
+        //defined variables
+        const formData = new FormData(event.target);
+        const bug = formData.get('bug').toLowerCase();
+        const $imgScreen = $('.screen-img');
+        const $result = $('.screen-txt');
+        
+        
+        const url = (`https://acnhapi.com/v1/bugs/` + bug)
+        console.log(url)
+
+     
+        
+
+        $.ajax(url)
+                .then((data) =>  {
+                        console.log(data)
+                        $result.html(`
+                        <div>
+                                ${data.id}
+                        </div>
+                        `)
+                        $imgScreen.html(`<img src=${data.image_uri} alt=${data.name}>`)
+                })
+                
+
+        
+     
+ 
+})
+
+
+
