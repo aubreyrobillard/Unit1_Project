fcc84a667fe714ead064402d0a1bfc8516c731fe
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-06-01 18:26:59 -0500

    Getting there

--- app.js
+++ app.js
@@ -21,17 +21,34 @@ $searchForm.on('submit', event => {
         const url = (`https://acnhapi.com/v1/bugs/` + bug)
         console.log(url)
 
-     
+
         
 
         $.ajax(url)
                 .then((data) =>  {
                         console.log(data)
                         $result.html(`
-                        <div>
-                                ${data.id}
+                        <div class="result">
+                                <b>Name:&nbsp;</b>${data['file-name']}
+                        </div>
+                        <div class="result">
+                                <b>ID:&nbsp;</b>${data.id}
+                        </div>
+                        <div class="result">
+                                <b>Location:&nbsp;</b>${data.availability.location}
+                        </div>
+                        <div class="result">
+                                <b>Rarity:&nbsp;</b>${data.availability.rarity}  
+                        </div>
+                        <div class="result">
+                                <b>Price:&nbsp;</b>${data.price}
+                        </div>
+                        <div class="result">
+                                <!-- notes: square brackets for special character naming--!>
+                                <b>Phrase:&nbsp;</b>${data['catch-phrase']}
                         </div>
                         `)
+
                         $imgScreen.html(`<img src=${data.image_uri} alt=${data.name}>`)
                 })
                 
